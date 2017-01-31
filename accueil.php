8e1c65094048624720349bcfbf3f96f8e8369b82
Sylvain Iseppi <sylvain.iseppi@gmail.com>
2017-01-31 08:18:14 +0100

    update accueil

--- accueil.php
+++ accueil.php
@@ -8,9 +8,9 @@
 	</div>
 	Je suis un jeune développeur Full-Stack passionné par le développement web. Ayant obtenu un BTS SIO je me suis inscrit a une session de formation développeur Full-stack chez M2IFormation afin de mieux cerner tous les aspects d'une application web et de me perfectionner dans le développement web qui est ma passion. 
 	<br />
-	Amateur de défis, je souhaite mener des projets réels et de grande envergure au sein d'une équipe dynamique. Je souhaite désormais travailler dans le développement d'application web afin de meler passion et travail.
+	Amateur de défis, je souhaite mener des projets réels et de grande envergure au sein d'une équipe dynamique. Je souhaite désormais travailler dans le développement d'application web afin d'accorder passion et travail.
 	<br />
-	Pour moi l'entreprise idéal posséde une bonne ambiance 
+	Pour moi l'entreprise idéale posséde un réel esprit d'équipe tout en gardant une autonomie dans les projets avec des possibilités de formation et d'évolution.
 
 
 
